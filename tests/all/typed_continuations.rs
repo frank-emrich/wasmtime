72a121975e02db50e6853c341c6333554abc48a0
Frank Emrich <git@emrich.io>
2024-02-27 20:56:28 +0000

    [testing] update tests

--- tests/all/typed_continuations.rs
+++ tests/all/typed_continuations.rs
@@ -3,6 +3,8 @@ use wasmtime::*;
 
 mod test_utils {
     use anyhow::{bail, Result};
+    use std::any::*;
+    use std::panic::AssertUnwindSafe;
     use wasmtime::*;
 
     pub struct Runner {
@@ -52,6 +54,22 @@ mod test_utils {
 
             assert_eq!(err.downcast_ref::<&'static str>(), Some(&error_message));
         }
+
+        pub fn run_test_expect_panic(
+            mut self,
+            wat: &str,
+            imports: &[Extern],
+        ) -> Box<dyn Any + Send + 'static> {
+            let module = Module::new(&self.engine, wat).unwrap();
+
+            let instance = Instance::new(&mut self.store, &module, imports).unwrap();
+            let entry = instance.get_func(&mut self.store, "entry").unwrap();
+
+            std::panic::catch_unwind(AssertUnwindSafe(|| {
+                drop(entry.call(&mut self.store, &mut [], &mut []))
+            }))
+            .unwrap_err()
+        }
     }
 
     /// Creates a simple Host function that increments an i32
@@ -95,6 +113,10 @@ mod test_utils {
                 Ok(())
             },
         )
+    }
+
+    pub fn make_panicking_host_func(store: &mut Store<()>, msg: &'static str) -> Func {
+        Func::wrap(store, move || std::panic::panic_any(msg))
     }
 }
 
@@ -357,7 +379,8 @@ fn inter_instance_suspend() -> Result<()> {
     Ok(())
 }
 
-/// Tests interaction with host functions.
+/// Tests interaction with host functions. Note that the interaction with host
+/// functions and traps is covered by the module `traps` further down.
 mod host {
     use super::test_utils::*;
     use wasmtime::*;
@@ -645,301 +668,8 @@ mod host {
     }
 }
 
-#[cfg(test)]
-#[cfg_attr(
-    feature = "typed_continuations_baseline_implementation",
-    allow(dead_code)
-)]
-mod helpers {
-    use std::panic::AssertUnwindSafe;
-
-    use anyhow::{bail, Result};
-    use std::any::*;
-    use wasmtime::*;
-
-    pub struct Runner {
-        pub engine: Engine,
-        pub store: Store<()>,
-    }
-
-    impl Runner {
-        pub fn new() -> Runner {
-            let mut config = Config::default();
-            config.wasm_function_references(true);
-            config.wasm_exceptions(true);
-            config.wasm_typed_continuations(true);
-
-            let engine = Engine::new(&config).unwrap();
-
-            let store = Store::<()>::new(&engine, ());
-
-            Runner { engine, store }
-        }
-
-        /// Uses this `Runner` to run the module defined in `wat`, satisfying
-        /// its imports using `imports`. The module must export a function
-        /// `entry`, taking no parameters and returning `Results`.
-        pub fn run_test<Results: WasmResults>(
-            mut self,
-            wat: &str,
-            imports: &[Extern],
-        ) -> Result<Results> {
-            let module = Module::new(&self.engine, wat)?;
-
-            let instance = Instance::new(&mut self.store, &module, imports)?;
-            let entry = instance.get_typed_func::<(), Results>(&mut self.store, "entry")?;
-
-            entry.call(&mut self.store, ())
-        }
-
-        pub fn run_test_expect_panic(
-            mut self,
-            wat: &str,
-            imports: &[Extern],
-        ) -> Box<dyn Any + Send + 'static> {
-            let module = Module::new(&self.engine, wat).unwrap();
-
-            let instance = Instance::new(&mut self.store, &module, imports).unwrap();
-            let entry = instance.get_func(&mut self.store, "entry").unwrap();
-
-            std::panic::catch_unwind(AssertUnwindSafe(|| {
-                drop(entry.call(&mut self.store, &mut [], &mut []))
-            }))
-            .unwrap_err()
-        }
-    }
-
-    /// Creates a simple Host function that increments an i32
-    pub fn make_i32_inc_host_func(runner: &mut Runner) -> Func {
-        Func::new(
-            &mut runner.store,
-            FuncType::new(&runner.engine, vec![ValType::I32], vec![ValType::I32]),
-            |mut _caller, args: &[Val], results: &mut [Val]| {
-                let res = match args {
-                    [Val::I32(i)] => i + 1,
-                    _ => bail!("Error: Received illegal argument (should be single i32)"),
-                };
-                results[0] = Val::I32(res);
-                Ok(())
-            },
-        )
-    }
-
-    /// Creates a host function of type i32 -> i32. `export_func` must denote an
-    /// exported function of type i32 -> i32. The created host function
-    /// increments its argument by 1, passes it to the exported function, and in
-    /// turn increments the result before returning it as the overall result.
-    pub fn make_i32_inc_via_export_host_func(
-        runner: &mut Runner,
-        export_func: &'static str,
-    ) -> Func {
-        Func::new(
-            &mut runner.store,
-            FuncType::new(&runner.engine, vec![ValType::I32], vec![ValType::I32]),
-            |mut caller, args: &[Val], results: &mut [Val]| {
-                let export = caller
-                    .get_export(export_func)
-                    .ok_or(anyhow::anyhow!("could not get b"))?;
-                let func = export
-                    .into_func()
-                    .ok_or(anyhow::anyhow!("b is not a Func"))?;
-                let func_typed = func.typed::<i32, i32>(caller.as_context())?;
-                let arg = args[0].unwrap_i32();
-                let res = func_typed.call(caller.as_context_mut(), arg + 1)?;
-                results[0] = Val::I32(res + 1);
-                Ok(())
-            },
-        )
-    }
-
-    pub fn make_panicking_host_func(store: &mut Store<()>, msg: &'static str) -> Func {
-        Func::wrap(store, move || std::panic::panic_any(msg))
-    }
-}
-
-/// Tests interaction with host functions. Note that the interaction with host
-/// functions and traps is covered by the module `traps` further down.
-mod host {
-    use super::helpers::*;
-    use wasmtime::*;
-
-    #[test]
-    /// Tests calling a host function from within a wasm function running inside a continuation.
-    /// Call chain:
-    /// $entry -resume-> a -call-> host_func_a
-    fn call_host_from_continuation() -> Result<()> {
-        let wat = r#"
-        (module
-            (type $ft (func (result i32)))
-            (type $ct (cont $ft))
-
-            (import "" "" (func $host_func_a (param i32) (result i32)))
-
-            (func $a (export "a") (result i32)
-                (call $host_func_a (i32.const 122))
-            )
-            (func $entry (export "entry") (result i32)
-                (resume $ct (cont.new $ct (ref.func $a)))
-            )
-        )
-    "#;
-
-        let mut runner = Runner::new();
-
-        let host_func_a = make_i32_inc_host_func(&mut runner);
-
-        let result = runner.run_test::<i32>(wat, &[host_func_a.into()]).unwrap();
-        assert_eq!(result, 123);
-        Ok(())
-    }
-
-    #[test]
-    /// Similar to `call_host_from_continuation`. However, the host function in turn
-    /// calls into wasm and runs a continuation.
-    /// Call chain:
-    /// $entry -resume-> $a -call-> $host_func_a -call-> $b -resume-> $c
-    fn call_host_from_continuation_nested_ok() -> Result<()> {
-        let wat = r#"
-        (module
-            (type $ft (func (param i32) (result i32)))
-            (type $ct (cont $ft))
-
-            (import "" "" (func $host_func_a (param i32) (result i32)))
-
-
-            (func $a (export "a") (param $x i32) (result i32)
-                (call $host_func_a (local.get $x))
-            )
-
-
-            (func $b (export "b") (param $x i32) (result i32)
-                (resume $ct (local.get $x) (cont.new $ct (ref.func $c)))
-            )
-
-            (func $c (export "c") (param $x i32) (result i32)
-                (return (i32.add (local.get $x) (i32.const 1)))
-            )
-
-
-            (func $entry (export "entry") (result i32)
-                (resume $ct (i32.const 120) (cont.new $ct (ref.func $a)))
-            )
-        )
-    "#;
-
-        let mut runner = Runner::new();
-
-        let host_func_a = make_i32_inc_via_export_host_func(&mut runner, "b");
-
-        let result = runner.run_test::<i32>(wat, &[host_func_a.into()]).unwrap();
-        assert_eq!(result, 123);
-        Ok(())
-    }
-
-    #[test]
-    /// Similar to `call_host_from_continuation_nested_ok`. However, after
-    /// crossing from the host back into wasm, we suspend to a tag that is
-    /// handled by the surrounding function (i..e, without needing to cross the
-    /// host frame to reach the handler).
-    /// Call chain:
-    /// $entry -resume-> $a -call-> $host_func_a -call-> $b -resume-> $c
-    fn call_host_from_continuation_nested_suspend_ok() -> Result<()> {
-        let wat = r#"
-        (module
-            (type $ft (func (param i32) (result i32)))
-            (type $ct (cont $ft))
-            (tag $t (result i32))
-
-            (import "" "" (func $host_func_a (param i32) (result i32)))
-
-
-            (func $a (export "a") (param $x i32) (result i32)
-                (call $host_func_a (local.get $x))
-            )
-
-
-            (func $b (export "b") (param $x i32) (result i32)
-                (block $h (result (ref $ct))
-                  (resume $ct (tag $t $h) (local.get $x) (cont.new $ct (ref.func $c)))
-                  (unreachable)
-                )
-                (drop)
-                ;; note that we do not run the continuation to completion
-                (i32.add (local.get $x) (i32.const 1))
-            )
-
-            (func $c (export "c") (param $x i32) (result i32)
-                (suspend $t)
-            )
-
-
-            (func $entry (export "entry") (result i32)
-                (resume $ct (i32.const 120) (cont.new $ct (ref.func $a)))
-            )
-        )
-    "#;
-
-        let mut runner = Runner::new();
-
-        let host_func_a = make_i32_inc_via_export_host_func(&mut runner, "b");
-
-        let result = runner.run_test::<i32>(wat, &[host_func_a.into()]).unwrap();
-        assert_eq!(result, 123);
-        Ok(())
-    }
-
-    #[ignore] // Needs clarification about intended behavior. Do host functions act as barriers for suspensions?
-    #[test]
-    /// Similar to `call_host_from_continuation_nested_suspend_ok`. However,
-    /// we suspend to a tag that is only handled if we were to cross a host function
-    /// boundary.
-    /// $entry -resume-> $a -call-> $host_func_a -call-> $b -resume-> $c
-    fn call_host_from_continuation_nested_suspend_unhandled() -> Result<()> {
-        let wat = r#"
-        (module
-            (type $ft (func (param i32) (result i32)))
-            (type $ct (cont $ft))
-            (tag $t (result i32))
-
-            (import "" "" (func $host_func_a (param i32) (result i32)))
-
-
-            (func $a (export "a") (param $x i32) (result i32)
-                (call $host_func_a (local.get $x))
-            )
-
-
-            (func $b (export "b") (param $x i32) (result i32)
-                (resume $ct (local.get $x) (cont.new $ct (ref.func $c)))
-            )
-
-            (func $c (export "c") (param $x i32) (result i32)
-                (suspend $t)
-            )
-
-
-            (func $entry (export "entry") (result i32)
-                (block $h (result (ref $ct))
-                  (return (resume $ct (tag $t $h) (i32.const 123) (cont.new $ct (ref.func $a))))
-                )
-                (drop)
-                (i32.const 123)
-            )
-        )
-    "#;
-
-        let mut runner = Runner::new();
-
-        let host_func_a = make_i32_inc_via_export_host_func(&mut runner, "b");
-
-        let result = runner.run_test::<i32>(wat, &[host_func_a.into()]).unwrap();
-        assert_eq!(result, 123);
-        Ok(())
-    }
-}
-
 mod traps {
-    use super::helpers::*;
+    use super::test_utils::*;
     use wasmtime::*;
 
     fn run_test_expect_trap_backtrace(wat: &str, expected_trap: Trap, expected_backtrace: &[&str]) {
